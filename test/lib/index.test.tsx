5f300ff849a6f534ead230a5ca22fb06b8c96760
Eyelly Wu <eyelly.wu@gmail.com>
2023-05-05 23:59:00 +0800

    test(Header): with id

--- test/lib/index.test.tsx
+++ test/lib/index.test.tsx
@@ -57,29 +57,60 @@ describe('test component render', () => {
     })
   })
 
-  it('Header', () => {
-    const title = 'Hellow World!'
-    const res = render(
-      <>
-        <H1>{title}</H1>
-        <H2>{title}</H2>
-        <H3>{title}</H3>
-        <H4>{title}</H4>
-        <H5>{title}</H5>
-        <H6>{title}</H6>
-      </>,
-    )
+  describe('Header', () => {
+    it('normal', () => {
+      const title = 'Hello World!'
+      const res = render(
+        <>
+          <H1>{title}</H1>
+          <H2>{title}</H2>
+          <H3>{title}</H3>
+          <H4>{title}</H4>
+          <H5>{title}</H5>
+          <H6>{title}</H6>
+        </>,
+      )
 
-    const expectRes = Array(6)
-      .fill(0)
-      .reduce((res, item, i) => {
-        res += `\n${Array(i + 1)
-          .fill('#')
-          .join('')} ${title}\n`
-        return res
-      }, '')
+      const expectRes = Array(6)
+        .fill(0)
+        .reduce((res, item, i) => {
+          res += `\n${Array(i + 1)
+            .fill('#')
+            .join('')} ${title}\n`
+          return res
+        }, '')
 
-    expect(res).toBe(expectRes)
+      expect(res).toBe(expectRes)
+    })
+
+    it('with id', () => {
+      const title = 'Hello World!'
+      const res = render(
+        <>
+          <H1 id="1">{title}</H1>
+          <H2 id="2">{title}</H2>
+          <H3 id="3">{title}</H3>
+          <H4 id="4">{title}</H4>
+          <H5 id="5">{title}</H5>
+          <H6 id="6">{title}</H6>
+        </>,
+      )
+
+      const expectRes = Array(6)
+        .fill(0)
+        .reduce((res, item, i) => {
+          const number = i + 1
+          res += `\n<h${number} id="${number}">${title}</h${number}>\n`
+          return res
+        }, '')
+
+      console.log({
+        res,
+        expectRes,
+      })
+
+      expect(res).toBe(expectRes)
+    })
   })
 
   it('Font', () => {
